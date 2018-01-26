359fcd300b6c6c81a3352500124a2e30b1bf0bbf
gross-micah <gross.micah@gmail.com>
2018-01-26 13:01:42 -0800

    Initial Commit

---
+++ RainbowTabs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RainbowTabs
+//
+//  Created by Micah Grossman on 1/26/18.
+//  Copyright © 2018 Micah Grossman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
